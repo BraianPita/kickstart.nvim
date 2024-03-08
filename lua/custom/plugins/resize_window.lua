80380495987de8ddea53856cdcf87c116dc68499
braian.pita <braian.pita@antemodal.com>
2024-03-08 09:21:03 -0600

    fix invalid 'spec' issue with resize_window module

--- lua/custom/plugins/resize_window.lua
+++ lua/custom/plugins/resize_window.lua
@@ -2,3 +2,5 @@ vim.keymap.set('n', '+', [[<cmd>vertical resize +1<cr>]]) -- make the window big
 vim.keymap.set('n', '_', [[<cmd>vertical resize -1<cr>]]) -- make the window smaller vertically
 vim.keymap.set('n', '=', [[<cmd>horizontal resize +1<cr>]]) -- make the window bigger horizontally by pressing shift and =
 vim.keymap.set('n', '-', [[<cmd>horizontal resize -1<cr>]]) -- make the window smaller horizontally by pressing shift and -
+
+return {}
