92077982a8b88df22c1cd2f7810065487af17315
Braian Pita <braianx100@gmail.com>
2024-03-08 02:05:16 -0600

    added project addon, resize keymaps, and tab lines plugins

---
+++ lua/custom/plugins/resize_window.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '+', [[<cmd>vertical resize +1<cr>]]) -- make the window biger vertically
+vim.keymap.set('n', '_', [[<cmd>vertical resize -1<cr>]]) -- make the window smaller vertically
+vim.keymap.set('n', '=', [[<cmd>horizontal resize +1<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set('n', '-', [[<cmd>horizontal resize -1<cr>]]) -- make the window smaller horizontally by pressing shift and -
